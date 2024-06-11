d197bc169cd5007cb58e03db66bb817dbaab47b8
MindaAndreea <165321457+MindaAndreea@users.noreply.github.com>
2024-06-11 09:02:04 +0300

    Main Nav and Side Nav

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburgerContainer = document.querySelector("#header-main-nav");
+
+const hamburger = document.querySelector(".header-main-nav-menu");
+
+const links = document.querySelectorAll(".header-main-nav-links li");
+
+hamburger.addEventListener("click", () => {
+  hamburgerContainer.classList.toggle("clicked");
+
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+});
